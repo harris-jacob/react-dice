ef1e3aba99f6efce299b5bf24e8a3f6f30874abd
harris-jacob <65860128+harris-jacob@users.noreply.github.com>
2022-07-13 21:17:30 +0100

    Demo: add max toast  (#61)

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import Toaster, { ToastList } from './components/Toaster'
 import './index.css'
 import React, { useCallback, useState } from 'react'
+import { useInterval } from './useInterval'
 
 let id = 0
 
 const getId = () => `${id++}`
+const MAX_TOAST = 3
+const TOAST_TIMEOUT = 100000
+
 
 const App = () => {
   // TODO auto timeout & max toast
@@ -19,6 +23,27 @@ const App = () => {
 
   const pushToast = useCallback(
     (result: number) =>
+      setToasts((toasts) => {
+        const newToasts = [
+          ...toasts,
+          {
+            id: getId(),
+            result,
+            timestamp: Date.now()
+          }
+        ]
+
+        return newToasts.length > MAX_TOAST ? newToasts.splice(1) : newToasts
+      }),
+    []
+  )
+
+  useInterval(() => {
+    setToasts((s) =>
+      s.filter((toast) => toast.timestamp + TOAST_TIMEOUT > Date.now())
+    )
+  }, TOAST_TIMEOUT)
+
       setToasts((toasts) => [
         ...toasts,
         {
@@ -29,7 +54,7 @@ const App = () => {
       ]),
     []
   )
-
+  
   return (
     <>
       <Toaster toasts={toasts} dismiss={dismissToast} />
